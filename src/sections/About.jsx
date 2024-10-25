448cd3fd6d8f43b68b01a10069b493787e8b8e56
nikgrbn <Rmaps123@gmail.com>
2024-10-25 13:25:14 +0300

    first span

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -7,8 +7,18 @@ function About () {
         <div className="col-span-1 xl:row-span-3">
           <div className="grid-container">
             <img src="/assets/grid1.png" alt="grid-1" className="w-full sm:h-[276px] h-fit object-contain"/>
+
+            <div>
+              <p className="grid-headtext">Hi, I'm Nikita</p>
+              <p className="grid-subtext">Since childhood programming was my passion.
+                My main focus is full stack development of web and native applications.
+              </p>
+            </div>
           </div>
         </div>
+
+
+
       </div>
     </section>
   )
