ab00eea725745d22b87224a8e8c30a050b95aeb5
nikgrbn <Rmaps123@gmail.com>
2024-10-26 16:55:06 +0300

    phone display with decal

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -4,6 +4,7 @@ import { Canvas } from '@react-three/fiber'
 import { Center, OrbitControls } from '@react-three/drei'
 import CanvasLoader from '../components/CanvasLoader.jsx'
 import DemoComputer from '../components/DemoComputer.jsx'
+import Smartphone from '../components/Smartphone.jsx'
 
 const projectCount = myProjects.length
 
@@ -75,14 +76,15 @@ function Projects () {
           </div>
         </div>
 
-        <div className="border border-black-300 bg-black-200 rounded-lg h-96 md:h-full">
+        <div className="border border-black-300 bg-black-200 rounded-lg h-96 ">
           <Canvas>
-            <ambientLight intensity={Math.PI}/>
+            <ambientLight intensity={2}/>
             <directionalLight position={[10, 10, 5]}/>
             <Center>
               <Suspense fallback={<CanvasLoader/>}>
-                <group scale={2} position={[0, -3, 0]} rotation={[0, -0.1, 0]}>
-                  <DemoComputer texture={currentProject.texture}/>
+                <group scale={1} position={[0, -3, 0]} rotation={[0, -0.1, 0]}>
+                  {/*<DemoComputer texture={currentProject.texture}/>*/}
+                  <Smartphone texture={currentProject.texture}/>
                 </group>
               </Suspense>
             </Center>
