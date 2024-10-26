99da7dba7c74d3dab799bcff8333c881f0f84a77
nikgrbn <Rmaps123@gmail.com>
2024-10-26 14:20:17 +0300

    demo-computer

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,5 +1,9 @@
-import React, { useState } from 'react'
+import React, { Suspense, useState } from 'react'
 import { myProjects } from '../constants/index.js'
+import { Canvas } from '@react-three/fiber'
+import { Center, OrbitControls } from '@react-three/drei'
+import CanvasLoader from '../components/CanvasLoader.jsx'
+import DemoComputer from '../components/DemoComputer.jsx'
 
 const projectCount = myProjects.length
 
@@ -69,8 +73,23 @@ function Projects () {
               <img src="/assets/right-arrow.png" alt="right-arrow" className="w-4 h-4"/>
             </button>
           </div>
-
         </div>
+
+        <div className="border border-black-300 bg-black-200 rounded-lg h-96 md:h-full">
+          <Canvas>
+            <ambientLight intensity={Math.PI}/>
+            <directionalLight position={[10, 10, 5]}/>
+            <Center>
+              <Suspense fallback={<CanvasLoader/>}>
+                <group scale={2} position={[0, -3, 0]} rotation={[0, -0.1, 0]}>
+                  <DemoComputer texture={currentProject.texture}/>
+                </group>
+              </Suspense>
+            </Center>
+            <OrbitControls maxPolarAngle={Math.PI/2} enableZoom={false}/>
+          </Canvas>
+        </div>
+
       </div>
     </section>
   )
